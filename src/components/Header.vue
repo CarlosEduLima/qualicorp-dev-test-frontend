862e3566eb68ec75c99f9034b710eab89fc27ea3
Carlos Eduardo <carlos.lima@sanp.com.br>
2021-05-02 14:05:27 -0300

    create basic template

---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="header">
+      <a href="#default" class="logo">Qualicorp Dev Test</a>
+      <div class="header-right">
+        <a href="#home">Meu perfil</a>
+        <a href="#contact">Sair</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  overflow: hidden;
+  background-color: #F7F8FC;
+  padding: 20px 10px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+
+}
+
+.header a {
+  float: left;
+  color: #5C5F69;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+  margin-right: 30px;
+  font-weight: bold;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+
+  .header-right {
+    float: none;
+  }
+}
+</style>
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,198 @@
+<template>
+  <div class="list-wrapper">
+  <ul class="list">
+    <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+      <li class="list-item">
+      <div>
+        <img src="/user.svg" class="list-item-image">
+      </div>
+      <div class="list-item-content">
+        <h4>Hitesh Kumar</h4>
+        <p>@hk-skit</p>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UsersTable"
+};
+</script>
+<style scoped>
+.list-wrapper {
+  max-width: 400px;
+  margin: 120px auto;
+}
+
+.list {
+  background: #F7F8FC;
+  border-radius: 2px;
+  list-style: none;
+  padding: 10px 20px;
+}
+
+.list-item {
+  display: flex;
+  margin: 10px;
+  padding-bottom: 5px;
+  padding-top: 5px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.list-item img{
+    border-radius: 50%;
+    width: 64px;
+}
+.list-item-content{
+   margin-left: 20px;
+}
+.list-item-content p{
+   margin: 0;
+   margin-top: 5px;
+   color: #717171;
+   float: left;
+}
+.list-item-content h4{
+  color: #212B36;
+    margin-top: 10px;
+    font-size: 18px;
+    margin-bottom: 5px;
+}
+
+</style>
